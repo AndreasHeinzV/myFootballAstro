87aafaa284852628d48fb0dcb609bb52d6f59c3c
andreasheinz <andreas.heinz@cec.valantic.com>
2025-02-07 15:18:34 +0100

    4/4 football controller finished.

--- Symfony-FootballApi/src/Components/Football/Communication/Controller/PlayerController.php
+++ Symfony-FootballApi/src/Components/Football/Communication/Controller/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Components\Football\Communication\Controller;
 
 use App\Components\Football\Business\FootballBusinessFacadeInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,14 +14,31 @@ class PlayerController extends AbstractController
     public function __construct(private readonly FootballBusinessFacadeInterface $footballBusinessFacade)
     {
     }
-    #[Route('player/{playerName}/{id}', name: 'player_details')]
-   // #[Route('team/{teamName}/player/{playerName}', name: 'player_details')]
-    public function index(Request $request): Response
-    {
+
+    #[Route('/player/{playerName}/{playerId}', name: 'player_details')]
+    // #[Route('team/{teamName}/player/{playerName}', name: 'player_details')]
+    public function index(string $playerId): JsonResponse {
+
+        $player = $this->footballBusinessFacade->getPlayer($playerId);
 
 
-        return $this->render('football/player_details.html.twig', [
-            'playerData' => $this->footballBusinessFacade->getPlayer($request->query->get('id')),
-        ]);
+        if (null === $player) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'No player found.',
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = [
+            'name' => $player->name,
+            'position' => $player->position,
+            'dateOfBirth' => $player->dateOfBirth,
+            'nationality' => $player->nationality,
+            'shirtNumber' => $player->shirtNumber,
+        ];
+        return new JsonResponse([
+            'status' => 'success',
+            'data' => $data,
+        ], Response::HTTP_OK);
     }
 }
